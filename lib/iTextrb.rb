f4085ae44348bfe85e510c2f6656ec0cfd4443fa
Christopher Ostrowski <chris@madebyfunction.com>
2014-02-24 23:04:44 -0600

    Minor file name change.

--- lib/iTextrb.rb
+++ lib/iTextrb.rb
@@ -1,4 +1,13 @@
 require "iTextRB/version"
+require 'java'
+require 'jars/itextpdf-5.5.0.jar'
+
+module IText
+  module JavaIO; include_package "java.io"; end
+  module PDF;    include_package "com.itextpdf.text.pdf"; end
+  module Text;   include_package "com.itextpdf.text"; end
+  module Image;  include_package "com.itextpdf.text.image"; end
+end
 
 module ITextRB
   # Your code goes here...
